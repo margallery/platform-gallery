77ca7349e2f01152023ac36fa569a357de3dc4d5
Ralph Bruens <Ralphbruens@hotmail.com>
2021-12-09 14:58:59 +0100

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,30 @@
-import 'nextra-theme-blog/style.css'
-import Head from 'next/head'
+import {ThemeProvider} from 'theme-ui'
+import theme from '../utils/theme'
+import '../styles/header.scss'
+import '../styles/layout.scss'
+import '../styles/footer.scss'
+import '../styles/component.scss'
+import {initGA, trackGAEvent} from '../utils/googleAnalytics'
 
-import '../styles/main.css'
-
-export default function Nextra({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
-        <link
-          rel="preload"
-          href="/fonts/Inter-roman.latin.var.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
+// for more info on measuring app performance
+// visit https://nextjs.org/docs/advanced-features/measuring-performance
+export function reportWebVitals(metric) {
+  initGA()
+  const {id, name, label, value} = metric
+  trackGAEvent(
+    'Web Vitals',
+    'NextJS Custom Metric',
+    name,
+    Math.round(name === 'CLS' ? value * 1000 : value)
   )
 }
+
+function MyApp({Component, pageProps}) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -1,35 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+import {InitializeColorMode} from 'theme-ui'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
-
   render() {
-    const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
-      image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
-    }
-
     return (
-      <Html lang="en">
-        <Head>
-          <meta name="robots" content="follow, index" />
-          <meta name="description" content={meta.description} />
-          <meta property="og:site_name" content={meta.title} />
-          <meta property="og:description" content={meta.description} />
-          <meta property="og:title" content={meta.title} />
-          <meta property="og:image" content={meta.image} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@yourname" />
-          <meta name="twitter:title" content={meta.title} />
-          <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={meta.image} />
-        </Head>
+      <Html>
+        <Head />
         <body>
+          <InitializeColorMode />
           <Main />
           <NextScript />
         </body>
